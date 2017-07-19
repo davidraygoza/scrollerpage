b6c191e0ad3318a8910184d2bc0865ad689cab5e
David Raygoza <david.raygoza.ramirez@gmail.com>
2017-07-19 15:44:04 -0500

    Initial Commit

---
+++ 04_scoller_page/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  04_scoller_page
+//
+//  Created by David Raygoza on 19/07/17.
+//  Copyright © 2017 David Raygoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
